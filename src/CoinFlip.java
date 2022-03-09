96d96fd346790be981403e561c4624e56f9feb46
clara <clararbalmer@gmail.com>
2022-03-08 21:13:07 -0500

    input case insensitive, retry after invalid input

--- src/CoinFlip.java
+++ src/CoinFlip.java
@@ -1,14 +1,37 @@
 import java.util.Scanner;
 import java.util.Random;
 
+//additions: outputs time or times, not time(s), user input not case sensitive, retry after invalid input (heads/tails),
+//retry after invalid input (number of flips), 
 public class CoinFlip {
 
 	public static void main(String[] args) {
 		Scanner scnr = new Scanner(System.in);
-		System.out.print("Guess which will have more: heads or tails? ");
-		String headsOrTailsGuess = scnr.next();
-		System.out.print("How many times shall we flip a coin? ");
-		int numberOfFlips = scnr.nextInt();
+		String headsOrTailsGuess = "";
+		int numberOfFlips = -1;
+		
+		do {
+			System.out.print("Guess which will have more: heads or tails? ");
+			headsOrTailsGuess = scnr.next().toLowerCase();
+			if (!headsOrTailsGuess.equals("heads") && !headsOrTailsGuess.equals("tails")) {
+				System.out.println("Invalid input. Try again!");
+			}
+		} while (!headsOrTailsGuess.equals("heads") && !headsOrTailsGuess.equals("tails"));
+		
+		do {
+			System.out.print("How many times shall we flip a coin? ");
+			if (scnr.hasNextInt()) {
+				numberOfFlips = scnr.nextInt();
+				if (numberOfFlips < 1) {
+					System.out.println("Invalid input. Try again!");
+				}
+			} else {
+				System.out.println("Invalid input. Try again!");
+			}
+		} while (numberOfFlips < 1);
+		
+		
+		
 		System.out.println();
 		
 		int correctCount = 0;
@@ -36,7 +59,11 @@ public class CoinFlip {
 		}
 		
 		System.out.println();
-		System.out.println("Your guess, " + headsOrTailsGuess + ", came up " + correctCount + " time(s).");
+		if (correctCount == 1) {
+			System.out.println("Your guess, " + headsOrTailsGuess + ", came up " + correctCount + " time.");	
+		} else {
+			System.out.println("Your guess, " + headsOrTailsGuess + ", came up " + correctCount + " times.");
+		}
 		System.out.println("That's " + correctCount * 100 / numberOfFlips + "%.");
 		scnr.close();
 	}
